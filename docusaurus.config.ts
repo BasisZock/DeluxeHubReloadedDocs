04249ce805d74eae694115bc30a02706f7af1a80
Strafbefehl <Khalaf.a@outlook.de>
2025-01-07 18:55:35 +0100

    deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://strafbefehl.github.io/DeluxeHubReloadedDocs/',
+  url: 'https://strafbefehl.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/DeluxeHubReloadedDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
