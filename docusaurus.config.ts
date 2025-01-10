edbf2f1ae63ed17d7e212f444102fd683523a44b
Strafbefehl <Khalaf.a@outlook.de>
2025-01-10 15:16:24 +0100

    fix edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Strafbefehl/DeluxeHubReloadedDocs/tree/main/docs/',
+            'https://github.com/Strafbefehl/DeluxeHubReloadedDocs/tree/main/',
           routeBasePath: '/'
         },
         theme: {
